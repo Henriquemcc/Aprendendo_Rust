558fb84383abd5cb3fa88bc36a5736343d42490e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-09-01 15:02:57 -0300

    Adicionando .gitignore para que sejam ignorados os binários, e alterando o arquivo do MyIo

--- jogo/adivinhacao/src/main.rs
+++ jogo/adivinhacao/src/main.rs
@@ -1,49 +1,17 @@
 use rand::Rng;
 mod myio;
 
+///Esta funcao eh a funcao principal, que eh a primeira funcao chamada quando o programa inicia.
 fn main()
 {
     //Exibindo mensagem de bem vindo
     println!("Bem vindo ao jogo da adivinhação");
 
     //Obtendo a dificuldade
-    let mut dificuldade: u8;
-    loop
-    {
-        //Exibindo as dificuldades
-        println!("Selecione a dificuldade:");
-        println!("(1) Fácil");
-        println!("(2) Médio");
-        println!("(3) Difícil");
-
-        //Obtendo a dificuldade
-        dificuldade= myio::read_u8();
-
-        if dificuldade < 1 || dificuldade > 3
-        {
-            eprintln!("Valor inválido");
-        }
-        else
-        {
-            break;
-        }
-    }
+    let dificuldade = obter_dificuldade();
 
     //Configurando a quantidade total de tentativas de acordo com a dificuldade escolhida
-    let mut quantidade_total_de_tentativas: u8=0;
-
-    if dificuldade==1
-    {
-        quantidade_total_de_tentativas=20;
-    }
-    else if dificuldade == 2
-    {
-        quantidade_total_de_tentativas=10;
-    }
-    else if dificuldade== 3
-    {
-        quantidade_total_de_tentativas=5;
-    }
+    let quantidade_total_de_tentativas = obter_quantidade_de_tentativas(dificuldade);
 
     //Gerando um numero secreto
     let numero_secreto:u8 = rand::thread_rng().gen_range(1, 101);
@@ -51,29 +19,14 @@ fn main()
     //Declarando pontos
     let mut pontos: i16 = 1000;
 
+    //Rodando a rodada
     for rodada in 1..quantidade_total_de_tentativas
     {
+        //Imprimindo o numero da tentativa
         println!("Tentativa {} de {}", rodada, quantidade_total_de_tentativas);
 
         //Obtendo o chute do usuario
-        let mut chute:u8;
-        loop
-        {
-            println!("Digite um numero entre 1 e 100");
-            chute = myio::read_u8();
-
-            //Verificando se o chute eh invalido
-            if chute <1 || chute >100
-            {
-                //Imprimindo mensagem de erro
-                eprintln!("O chute deve ser entre 1 e 100");
-            }
-            else
-            {
-                //Saindo do loop
-                break;
-            }
-        }
+        let chute = obter_chute();
 
         //Verificando se o chute eh igual ao numero secreto
         if chute==numero_secreto
@@ -106,4 +59,82 @@ fn main()
     println!("O número secreto era {}. Você fez {} pontos", numero_secreto, pontos);
 
 
+}
+
+///Esta funcao serve para obter do usuario a dificuldade que ele deseja jogar.
+///Retorno: u8: Numero inteiro com o nivel de dificuldade.
+fn obter_dificuldade() -> u8
+{
+    //Obtendo a dificuldade
+    let mut dificuldade: u8;
+    loop
+    {
+        //Exibindo as dificuldades
+        println!("Selecione a dificuldade:");
+        println!("(1) Fácil");
+        println!("(2) Médio");
+        println!("(3) Difícil");
+
+        //Obtendo a dificuldade
+        dificuldade= myio::read_u8();
+
+        if dificuldade < 1 || dificuldade > 3
+        {
+            eprintln!("Valor inválido");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return dificuldade;
+}
+
+///Esta funcao serve para obter a quantidade total de tentativas com base na dificuldade do jogo.
+///Parametro: dificuldade: Numero inteiro de 1 a 3 indicando a dificuldade do jogo (1-> facil, 2-> medio, 3->dificil).
+///Retorno: u8: Numero inteiro indicando a quantidade total de tentativas que o usuario tem direito.
+fn obter_quantidade_de_tentativas(dificuldade: u8) -> u8
+{
+    let mut quantidade_total_de_tentativas: u8=0;
+
+    if dificuldade==1
+    {
+        quantidade_total_de_tentativas=20;
+    }
+    else if dificuldade == 2
+    {
+        quantidade_total_de_tentativas=10;
+    }
+    else if dificuldade== 3
+    {
+        quantidade_total_de_tentativas=5;
+    }
+
+    return quantidade_total_de_tentativas;
+}
+
+///Esta funcao serve para obter o chute do usuario.
+///Retorno: u8: Numero inteiro do chute do usuario.
+fn obter_chute() -> u8
+{
+    let mut chute:u8;
+    loop
+    {
+        println!("Digite um numero entre 1 e 100");
+        chute = myio::read_u8();
+
+        //Verificando se o chute eh invalido
+        if chute <1 || chute >100
+        {
+            //Imprimindo mensagem de erro
+            eprintln!("O chute deve ser entre 1 e 100");
+        }
+        else
+        {
+            //Saindo do loop
+            break;
+        }
+    }
+
+    return chute;
 }
